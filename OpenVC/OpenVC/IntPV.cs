02f508bfd9ac71c85b7313d4356ce84e06f0fc77
DatScam <38138501+DatScam@users.noreply.github.com>
2019-06-07 20:41:21 +0300

    Add files via upload

---
+++ OpenVC/OpenVC/IntPV.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenVC
+{
+    class IntPV
+    {
+        private static List<object> _data = new List<object>();
+        private static List<object> _vars = new List<object>();
+
+        public static void SetVariable(object variable, object data)
+        {
+            if (variable != null && data != null)
+            {
+                bool foundSet = false;
+                foreach (object s in _vars)
+                {
+                    if (s.Equals(variable)) foundSet = true;
+                }
+                if (!foundSet)
+                {
+                    _vars.Add(variable);
+                    _data.Add(data);
+                }
+                else
+                {
+                    int index = GetIndexAtVar(variable);
+                    if (index > -1)
+                    {
+                        _data.RemoveAt(index);
+                        _vars.RemoveAt(index);
+                        _data.Add(data);
+                        _vars.Add(variable);
+                    }
+                }
+            }
+        }
+
+        public static void RemoveVariable(object variable)
+        {
+            if (variable != null)
+            {
+                bool foundSet = false;
+                foreach (object s in _vars)
+                {
+                    if (s.Equals(variable)) foundSet = true;
+                }
+                if (foundSet)
+                {
+                    int index = GetIndexAtVar(variable);
+                    if (index > -1)
+                    {
+                        _data.RemoveAt(index);
+                        _vars.RemoveAt(index);
+                    }
+                }
+            }
+        }
+
+        public static object GetVariableData(object variable)
+        {
+            object result = null;
+            if (variable != null)
+            {
+                bool foundSet = false;
+                foreach (object s in _vars)
+                {
+                    if (s.Equals(variable)) foundSet = true;
+                }
+                if (foundSet)
+                {
+                    int index = GetIndexAtVar(variable);
+                    if (index > -1)
+                    {
+                        result = _data[index];
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static int GetIndexAtVar(object obj)
+        {
+            int result = -1;
+            for (int i = 0; i < _vars.Count; i++)
+            {
+                if (_vars[i].Equals(obj)) result = i;
+            }
+            return result;
+        }
+
+    }
+}
